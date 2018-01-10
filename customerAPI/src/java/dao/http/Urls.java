51f5daadc600764cb64dc9d214e7940b2883ed9f
Agibert <matheusagibert@gmail.com>
2018-01-10 16:30:09 -0200

    update queue-lib

--- customerAPI/src/java/dao/http/Urls.java
+++ customerAPI/src/java/dao/http/Urls.java
@@ -12,7 +12,7 @@ package dao.http;
 public enum Urls {
 
     CADASTRO_STEALER("http://10.40.195.81:8080/stealerAPI/oss/"),
-    FULLTEST("http://localhost:8080/fulltestAPI/fulltest/co"),
+    FULLTEST("http://10.40.195.81:8080/fulltestAPI/fulltest/co"),
     QUEUE("http://localhost:8080/queueAPI/queue/");
 
     private final String valor;
