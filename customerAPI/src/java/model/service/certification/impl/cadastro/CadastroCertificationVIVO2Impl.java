26ea909c84c642fed0144422c97eeaa56bae37fd
Agibert <matheusagibert@gmail.com>
2018-01-15 21:42:03 -0200

    cadastroCertificationVivo2 in progress

--- customerAPI/src/java/model/service/certification/impl/cadastro/CadastroCertificationVIVO2Impl.java
+++ customerAPI/src/java/model/service/certification/impl/cadastro/CadastroCertificationVIVO2Impl.java
@@ -5,6 +5,10 @@
  */
 package model.service.certification.impl.cadastro;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.service.assertations.CustomerCertificationAsserter;
+import model.service.certification.enums.CertificationAssertName;
 import model.service.certification.enums.CertificationResult;
 
 public class CadastroCertificationVIVO2Impl extends CadastroCertification {
@@ -15,7 +19,12 @@ public class CadastroCertificationVIVO2Impl extends CadastroCertification {
     @Override
     protected void process() {
         if (this.getSubject().getRede() != null) {
-            
+            try {
+                this.getAsserts().add(new CustomerCertificationAsserter().assertCertification(CertificationAssertName.HAS_BLOQ_RADIUS, getSubject()));
+                this.getAsserts().add(new CustomerCertificationAsserter().assertCertification(CertificationAssertName.IS_TBS_EQUALS_RADIUS, getSubject()));
+            } catch (Exception ex) {
+                Logger.getLogger(CadastroCertificationVIVO2Impl.class.getName()).log(Level.SEVERE, null, ex);
+            }
             this.concluir(CertificationResult.OK, "Cadastro OK");
         } else {
             this.concluir(CertificationResult.FORWARDED_CO, "Cadastro NOK.");
