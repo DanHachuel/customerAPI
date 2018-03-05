c5ebfd6b208c54d3fe381f13d7cfcfc25ea51936
Henrique Merlin <henmerlin@gmail.com>
2018-03-05 10:16:01 -0300

    change to old impl.

--- customerAPI/src/main/java/br/net/gvt/efika/customerAPI/rest/impl/CustomerApiServiceImpl.java
+++ customerAPI/src/main/java/br/net/gvt/efika/customerAPI/rest/impl/CustomerApiServiceImpl.java
@@ -9,13 +9,14 @@ import br.net.gvt.efika.customerAPI.model.service.factory.FactoryService;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaResteasyServerCodegen", date = "2018-01-04T13:39:04.668Z")
 public class CustomerApiServiceImpl extends CustomerApiService {
-    
+
     @Override
     public Response findByParameter(GenericRequest body, SecurityContext securityContext)
             throws NotFoundException {
         try {
             // do some magic!
-            return Response.ok().entity(FactoryService.createEfikaCustomerService().consultar(body.getParameter())).build();
+            // return Response.ok().entity(FactoryService.createEfikaCustomerService().consultar(body.getParameter())).build();
+            return Response.ok().entity(FactoryService.customerFinder().getCustomer(body)).build();
         } catch (Exception ex) {
             ex.printStackTrace();
             return Response.serverError().entity(ex).build();
--- customerAPI/src/test/java/br/net/gvt/efika/customerAPI/dao/certification/CertificationDAOImplIT.java
+++ customerAPI/src/test/java/br/net/gvt/efika/customerAPI/dao/certification/CertificationDAOImplIT.java
@@ -8,6 +8,7 @@ package br.net.gvt.efika.customerAPI.dao.certification;
 import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 import java.util.List;
 import br.net.gvt.efika.customerAPI.model.entity.CustomerCertification;
+import br.net.gvt.efika.util.json.JacksonMapper;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,12 +44,12 @@ public class CertificationDAOImplIT {
         try {
             System.out.println("findByCustomer");
             EfikaCustomer cust = new EfikaCustomer();
-            cust.setInstancia("4130886762");
+            cust.setInstancia("4130157784");
             CertificationDAOImpl instance = new CertificationDAOImpl();
             List<CustomerCertification> result = instance.findByCustomer(cust);
+            System.out.println(new JacksonMapper<>(List.class).serialize(result));
             assertFalse(result.isEmpty());
         } catch (Exception e) {
-            
             e.printStackTrace();
             fail(e.getMessage());
         }
