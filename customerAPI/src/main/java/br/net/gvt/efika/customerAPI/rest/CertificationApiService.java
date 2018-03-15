b92973cfc57e755210e827043f41ad1aa635f7f0
Henrique Merlin <henmerlin@gmail.com>
2018-03-15 16:53:55 -0300

    test

--- customerAPI/src/main/java/br/net/gvt/efika/customerAPI/rest/CertificationApiService.java
+++ customerAPI/src/main/java/br/net/gvt/efika/customerAPI/rest/CertificationApiService.java
@@ -3,16 +3,17 @@ package br.net.gvt.efika.customerAPI.rest;
 import br.net.gvt.efika.customer.model.dto.GenericRequest;
 import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 import br.net.gvt.efika.customerAPI.model.CertificationResponse;
+import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaResteasyServerCodegen", date = "2018-01-04T13:39:04.668Z")
 public abstract class CertificationApiService {
 
-    public abstract Response certification(GenericRequest body, SecurityContext securityContext)
+    public abstract Response certification(@Valid GenericRequest body, SecurityContext securityContext)
             throws NotFoundException;
 
-    public abstract Response findByCustomer(EfikaCustomer body, SecurityContext securityContext)
+    public abstract Response findByCustomer(@Valid EfikaCustomer body, SecurityContext securityContext)
             throws Exception;
 
     public abstract Response getCertificationById(String id, SecurityContext securityContext)
